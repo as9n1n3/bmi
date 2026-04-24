8d0fd04409e0483c75f45f736b8e5d158476c92e
as9n1n3 <mashmas2003@gmail.com>
2026-04-24 20:52:55 +0300

    Add files via upload

--- practicum.py
+++ practicum.py
@@ -1,4 +1,4 @@
-# Шаг 1. Знакомство с пользователем
+
 print("Привет! Я бот-помощник для расчёта норм воды и калорий.")
 print("Давай познакомимся поближе.\n")
 
@@ -7,8 +7,8 @@ user_age = int(input("Сколько тебе лет? "))
 
 print(f"\n{user_name}, теперь мне нужно узнать твои параметры.")
 
-user_weight = float(input("Введи свой вес (в кг, например 75.5): "))
-user_height = float(input("Введи свой рост (в метрах, например 1.75): "))
+user_weight = float(input("Введи вес (кг, например 75.5): "))
+user_height = float(input("Введи рост (м, например 1.75): "))
 
 bmi = user_weight / (user_height ** 2)
 bmi_rounded = round(bmi, 1)
@@ -19,6 +19,4 @@ water_l = water_ml / 1000
 print("Результаты расчётов для", user_name,
       "(возраст:", user_age, "лет):")
 print("Твой ИМТ:", bmi_rounded)
-print("Твоя дневная норма воды:", round(water_l, 1),
-      "литров")
-print('Пейте больше воды, будьте здоровы!')
+print("Твоя норма воды:", round(water_l, 1), "литров")
